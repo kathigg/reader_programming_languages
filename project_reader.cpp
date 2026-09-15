d8c9535b78610165e55e7745fb0159921bfc787f
kathigg <kathigg@udel.edu>
2026-09-15 17:02:58 -0400

    first running version compelted

--- project_reader.cpp
+++ project_reader.cpp
@@ -1,6 +1,9 @@
 // for the READER project, programming languages 
 #include <iostream>
 #include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <cstring> // need it for memset
 using namespace std; 
 
 typedef int NUMBER;
@@ -13,93 +16,93 @@ const char* PROMPT2 = ">  ";
 const char COMMENTCHAR = ';';
 const int TABCODE = 9; // in ASCII, may or may not be right
 
-struct EXPLISTREC;
-typedef EXPLISTREC* EXPLIST;
-enum EXPTYPE { VALEXP, VAREXP, APEXP};
-enum EXPTYPE { VALEXP, VAREXP, APEXP };
-struct EXPREC {
-    EXPTYPE etype; // what type of expression
-    NUMBER num;
-    NAME variable; 
-};
-struct EXPLISTREC {
-    EXP head; 
-    EXPLIST tail;
-};
-struct VALUELISTREC {
-    NUMBER head; 
-    VALUELISTREC* tail;
-    typedef VALUELISTREC* VALUELIST;
-};
-struct NAMELISTREC {
-    NAME head; 
-    NAMELISTREC* tail;
-    typedef NAMELISTREC* NAMELIST;
-};
-struct ENVREC {
-    NAMELIST vars;
-    VALUELIST values;
-};
-typedef ENVREC* ENV; 
-struct FUNDEFREC {
-    NAME funname; 
-    NAMELIST formals;
-    EXP body; 
-    FUNDREC* nextfundef; 
-}; 
-typedef FUNDEFREC* FUNDEF; 
-FUNDEF fundefs; 
-ENV globalEnv; 
-EXP currentExp; 
+// struct EXPLISTREC;
+// typedef EXPLISTREC* EXPLIST;
+// enum EXPTYPE { VALEXP, VAREXP, APEXP};
+// enum EXPTYPE { VALEXP, VAREXP, APEXP };
+// struct EXPREC {
+//     EXPTYPE etype; // what type of expression
+//     NUMBER num;
+//     NAME variable; 
+// };
+// struct EXPLISTREC {
+//     EXP head; 
+//     EXPLIST tail;
+// };
+// struct VALUELISTREC {
+//     NUMBER head; 
+//     VALUELISTREC* tail;
+//     typedef VALUELISTREC* VALUELIST;
+// };
+// struct NAMELISTREC {
+//     NAME head; 
+//     NAMELISTREC* tail;
+//     typedef NAMELISTREC* NAMELIST;
+// };
+// struct ENVREC {
+//     NAMELIST vars;
+//     VALUELIST values;
+// };
+// typedef ENVREC* ENV; 
+// struct FUNDEFREC {
+//     NAME funname; 
+//     NAMELIST formals;
+//     EXP body; 
+//     FUNDREC* nextfundef; 
+// }; 
+// typedef FUNDEFREC* FUNDEF; 
+// FUNDEF fundefs; 
+// ENV globalEnv; 
+// EXP currentExp; 
 
-// ...
+// // ...
 
-EXP mkVALEXP(NUMBER n) {
-    EXP e;
-    e = new EXPREC;
-    e -> etype = VALEXP;
-    e -> num = n; 
-    return e; 
-} 
+// EXP mkVALEXP(NUMBER n) {
+//     EXP e;
+//     e = new EXPREC;
+//     e -> etype = VALEXP;
+//     e -> num = n; 
+//     return e; 
+// } 
 
-// ...
+// // ...
 
-int lengthVL(VALUELIST vl){
-    int i = 0;
-    while (vl != 0){
-        i++; 
-        vl = vl -> tail;
-    }
-    return i; 
-}
+// int lengthVL(VALUELIST vl){
+//     int i = 0;
+//     while (vl != 0){
+//         i++; 
+//         vl = vl -> tail;
+//     }
+//     return i; 
+// }
 
-int lengthML(NAME fname) {
-    FUNDEF f;
-    f = fundefs; 
-} 
+// int lengthML(NAME fname) {
+//     FUNDEF f;
+//     f = fundefs; 
+// } 
 
-// void newFunDef(NAME fname; NAMELIST nl; EXP e);
+// // void newFunDef(NAME fname; NAMELIST nl; EXP e);
 
-// init names -- place all pre-defined names ino printNames 
-void initNames() {
-    int i = 0; 
-    fundefs = 0; 
-    printNames[i] = (char*)"loop"; i++;
-    printNames[i] = (char*)"if"; i++
-    printNames[i] = (char*)"block"; i++
-    printNames[i] = (char*)"set"; i++
-    printNames[i] = (char*)"+"; i++
-    printNames[i] = (char*)"-"; i++
-    printNames[i] = (char*)"*"; i++
-    printNames[i] = (char*)"/"; i++
-    printNames[i] = (char*)"="; i++
-    printNames[i] = (char*)"<"; i++
-    printNames[i] = (char*)">"; i++
-    printNames[i] = (char*)"print"; i++
+// // init names -- place all pre-defined names ino printNames 
+// void initNames() {
+//     int i = 0; 
+//     fundefs = 0; 
+//     printNames[i] = (char*)"loop"; i++;
+//     printNames[i] = (char*)"if"; i++
+//     printNames[i] = (char*)"block"; i++
+//     printNames[i] = (char*)"set"; i++
+//     printNames[i] = (char*)"+"; i++
+//     printNames[i] = (char*)"-"; i++
+//     printNames[i] = (char*)"*"; i++
+//     printNames[i] = (char*)"/"; i++
+//     printNames[i] = (char*)"="; i++
+//     printNames[i] = (char*)"<"; i++
+//     printNames[i] = (char*)">"; i++
+//     printNames[i] = (char*)"print"; i++
 
-    numNames=i;
-    numBuiltIns = 1; 
-}; 
+//     numNames=i;
+//     numBuiltIns = 1; 
+// }; 
 
 void stripComments(char input[], int length){ 
     // changes all items with a comment on into whitespace 
@@ -110,6 +113,8 @@ void stripComments(char input[], int length){
         bool seenComment = false;
         if (input[i] == ';'){
             seenComment = true; 
+            input[i] = '#';
+            continue; 
         }
         if (seenComment == true) {
             // if we have already seen a comment, then
@@ -138,24 +143,80 @@ bool areParenthesesBalanced(char input[], int length)
     }
     return s.empty(); 
 };
-int processInput(char input[], int length) {
-    // returns a 1 if parenthesis match 
-    // returns a 0 if parenthesis don't match
-    stripComments(input, length); 
-    
 
-
+void issuePrompt(char input[], int length) {
+    stripComments(input, length); // remove comments from the input
+    bool matchingParens = areParenthesesBalanced(input, length); // stores whether parentheses match or not
+    if (matchingParens) {
+        // if parentheses match, issue prompt 1
+        std::cout << PROMPT;
+    }
+    else if (!matchingParens) {
+        // if parentheses don't match, issue prompt 2
+        std::cout << PROMPT2;
+    }
 };
 
 int main() {
-    initNames(); 
-    globalEnv = emptyEnv(); 
+    // initNames(); 
+    // globalEnv = emptyEnv(); 
     char input[MAXINPUT];
-    quittingtime = 0;
-    while !(quittingtime){ 
+    char newLine[MAXINPUT/2]; 
+    bool quittingtime = 0;
+    while ((std::cin.getline(newLine, MAXINPUT) && !quittingtime)){ 
         std::cout << PROMPT;
-        std::cin.getline(input,MAXINPUT); // as a reminder, this only stores one line, and it doesn't include the new line (stops @ new line)
-        processInput(input, (sizeof(input) / sizeof(input[0]))); 
+        std::cin.getline(newLine,MAXINPUT); // as a reminder, this only stores one line, and it doesn't include the new line (stops @ new line)
+        int newLineLength = (sizeof(newLine) / sizeof(newLine[0]));
+        int length = (sizeof(input) / sizeof(input[0])); 
+
+        // if the input says quit, then exit and terminate the program
+        if (strcmp(input, "quit") == 0) {
+            quittingtime = 1; 
+            break; 
+        }
+
+        // is the input string empty?
+        if (length == 0) {
+            strcpy(input, newLine); 
+        }
+        // is the input string not empty? 
+        else if (length > 0) {
+            if (length + newLineLength + 2 < MAXINPUT) {
+                // concatenate the old string and new string 
+                strcat(input, " "); 
+                strcat(input, newLine); 
+            }
+            else {
+                std::cout << "Buffer full!";
+                quittingtime = 1; 
+                break;
+        }
+
+        // if parentheses are balanced for the line, then issue the correct prompt and then clear the array
+        if (areParenthesesBalanced(input, length)) {
+            // issue the correct prompt
+            issuePrompt(input, length);
+            // clear the array
+            input[0] = '\0'; 
+            continue; 
+        }
+        // if parentheses are not balanced for the line, we won't overwrite the array
+        else if (!areParenthesesBalanced(input, length)) {
+            // issue the correct prompt
+            issuePrompt(input, length);
+            continue; 
+        }
+    }
+    } 
+
+
+
+
+
+    } 
+        
+        
+        
 
 
         
